14bb0bf4ebb0ae614df64da7f88b7bfbd42d2cff
bitxwb <103883559+bitxwb@users.noreply.github.com>
2022-08-19 15:22:53 +0800

    登录和注册
    改了登录和注册

--- GUI/register.cpp
+++ GUI/register.cpp
@@ -39,9 +39,42 @@ Register::~Register()
 
 void Register::on_confirmbtn_clicked()
 {
-    Login *l = new Login();
-    l->show();
-    this->close();
+    QString nickname = this->ui->le_name->text();//昵称
+    QString password1 = this->ui->le_setting->text();//密码
+    QString password2 = this->ui->le_ensure->text();//确认密码
+    QFile file0("C:/Users/86150/Desktop/QtPro819_1/restore/user.txt");
+    file0.open(QIODevice::ReadOnly);
+    QTextStream tread(&file0);
+    while(!tread.atEnd()){
+        QString tmp = tread.readLine();
+        if(nickname==tmp){
+            this->ui->label->setText("账号已存在");
+            file0.close();
+            return;
+        }
+    }
+
+    if(nickname==""){
+        this->ui->label->setText("昵称不能为空");
+    }
+    if(password1.length()<6){
+        this->ui->label->setText("密码长度小于六位");
+    }
+    if(password1!=password2){
+        this->ui->label->setText("两次密码输入不一致");
+        this->ui->le_setting->setText("");
+        this->ui->le_ensure->setText("");
+    }
+    //C:/Users/86150/Desktop/QtPro819_1/restore/user.txt"
+    else{//否则存入user.txt
+    QFile file("C:/Users/86150/Desktop/QtPro819_1/restore/user.txt");
+         if(file.open(QIODevice::WriteOnly|QIODevice::Append) == true) {
+                     QTextStream twrite(&file);
+                     twrite << nickname << endl << password1 << endl;
+                     this->ui->label->setText("注册成功！");
+         }
+
+    }
 }
 
 void Register::on_backbtn_clicked()
