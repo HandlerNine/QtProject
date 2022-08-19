d1bcc17064d9bc29d0d5917207e8e86dd6c93dd2
longestname <93931025+longestname@users.noreply.github.com>
2022-08-19 23:59:24 +0800

    添加chat，msg，userinfo类

--- Entity/msg.cpp
+++ Entity/msg.cpp
@@ -1,6 +1,39 @@
-//#include "msg.h"
+#include "msg.h"
 
-//Msg::Msg()
-//{
+Msg::Msg(MsgType _type,
+    const QByteArray& _content)
+    : type(_type)
+    , content(_content)
+{
+}
 
-//}
+QByteArray& Msg::getContent()
+{
+    return content;
+}
+
+/*
+ * Msg的QByteArray格式:
+ * quint32 datasize
+ * quint32 head
+ * quint16 type
+ * QByteArray content
+ */
+// 接收QByteArray流并转化成Msg信息
+Msg toMsg(const QByteArray& received_qbytearray)
+{
+    QDataStream in(received_qbytearray);
+    in.setVersion(QDataStream::Qt_5_8);
+
+    quint32 _datasize;
+    in >> _datasize;
+    quint32 _head;
+    in >> _head;
+
+    quint32 _type;
+    in >> _type;
+    QByteArray _content;
+    in >> _content;
+
+    return Msg((MsgType)(_type), _content);
+}
